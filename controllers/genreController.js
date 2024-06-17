0593918ad6f3ffdafd02a400d89a4fa9e314236b
Jeffrey Li <jli588@myseneca.ca>
2024-06-16 22:23:31 -0400

    init commit of Genre form validation and sanitization (GET, POST)

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -1,6 +1,7 @@
 const Genre = require("../models/genre");
 const Book = require("../models/book");
 const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
 
 // list all Genre
 exports.genre_list = asyncHandler(async (req, res, next) => {
@@ -32,13 +33,57 @@ exports.genre_detail = asyncHandler(async (req, res, next) => {
 
 // GET form to create Genre
 exports.genre_create_get = asyncHandler(async (req, res, next) => {
-  res.send(`NOT IMPLEMENTED: genre create GET`);
+  console.log({ errors: null });
+  res.render("genre_form", {
+    title: "Create Genre",
+    genre: null,
+    errors: null,
+  });
 });
 
 // POST form to handle create Genre
-exports.genre_create_post = asyncHandler(async (req, res, next) => {
-  res.send(`NOT IMPLEMENTED: genre create POST`);
-});
+exports.genre_create_post = [
+  // Validate and sanitize the name field.
+  body("name", "Genre name must contain at least 3 characters")
+    .trim()
+    .isLength({ min: 3 })
+    .escape(),
+
+  // Process request after validation and sanitization.
+  asyncHandler(async (req, res, next) => {
+    // res.send(`NOT IMPLEMENTED: genre create POST`);
+    // Extract the validation errors from a request.
+    const errors = validationResult(req);
+
+    // Create a genre object with escaped and trimmed data.
+    const genre = new Genre({ name: req.body.name });
+    if (!errors.isEmpty()) {
+      // There are errors. Render the form again with sanitized values/error messages.
+      console.log(`genre: ${genre}`);
+      errors.array().map((e) => console.log(e));
+      res.render("genre_form", {
+        title: "Create Genre",
+        genre: genre,
+        errors: errors.array(),
+      });
+      return;
+    } else {
+      // Data from form is valid.
+      // Check if Genre with same name already exists.
+      const genreExists = await Genre.findOne({ name: req.body.name })
+        .collation({ locale: "en", strength: 2 })
+        .exec();
+      if (genreExists) {
+        // Genre exists, redirect to its detail page.
+        res.redirect(genreExists.url);
+      } else {
+        await genre.save();
+        // New genre saved. Redirect to genre detail page.
+        res.redirect(genre.url);
+      }
+    }
+  }),
+];
 
 // GET form to DELETE Genre
 exports.genre_delete_get = asyncHandler(async (req, res, next) => {
